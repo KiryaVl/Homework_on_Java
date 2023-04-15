01f1a3ce6ba7f527a226416738336c37363bb006
Kirya <rabbitnya@gmail.com>
2023-04-15 18:49:36 +0300

    Added new HW PhoneBook on Java

---
+++ OOP/Task5/Phonebook/Config.java
@@ -0,0 +1,8 @@
+package HomeWorkTasks.OOP.Task5.Phonebook;
+
+public class Config {
+
+    public static String getPathDb() {
+        return "C:\\Users\\Vlaka\\IdeaProjects\\Homework\\src\\HomeWorkTasks\\OOP\\Task5\\Phonebook\\data.bd";
+    }
+}
